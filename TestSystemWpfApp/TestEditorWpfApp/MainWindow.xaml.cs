51822a5f82b16b89ead04ed9a46214079ed25f04
CozyWool <vsergeev201530@gmail.com>
2024-02-24 19:49:48 +0500

    Сделал часть разметки редактора тестов

--- TestSystemWpfApp/TestEditorWpfApp/MainWindow.xaml.cs
+++ TestSystemWpfApp/TestEditorWpfApp/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TestSystemWpfApp;
+namespace TestEditorWpfApp;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
